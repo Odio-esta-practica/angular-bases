828364ddfbdd8b1da778a16fc18ca3fa3ea422ba
Pablo Antonio Sandoval Perez <pabloantonio.sandoval@randstad.es>
2023-07-14 08:21:52 +0200

    Fin de sección 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-counter',
+    template: `
+        <h3>Counter: {{ counter }}</h3>
+        <button (click)="increaseBy(true)">+1</button>
+        <button (click)="resetCounter()">Reset</button>
+        <button (click)="increaseBy(false)">-1</button>
+    `,
+})
+export class CounterComponent {
+    public counter:number = 10;
+
+    increaseBy(value:boolean):void {
+      if(value)
+        this.counter += 1;
+      else
+        this.counter -= 1;
+  
+    }
+  
+    resetCounter():void {
+        this.counter = 10;
+    }
+}
