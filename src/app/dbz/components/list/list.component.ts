8816395a6df5470ec0bdd6af05274e979220f780
Pablo Antonio Sandoval Perez <pabloantonio.sandoval@randstad.es>
2023-07-26 17:16:22 +0200

    Docs creados

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList : Character[] = [{
+    id: uuid(),
+    name: 'Trunks',
+    power: 10
+  }];  
+  
+  onDeleteCharacter(id: string): void {
+    console.log(id)
+    this.onDelete.emit(id);
+  }
+}
